ca5b858c9fef4282f35f1ded4bbfe1a68add9b8a
ToshiKuwako <toshiki.se@gmail.com>
2020-08-29 16:08:01 +0900

    準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index #indexアクションの定義
-    @posts = Post.all 
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
+  
 
   def create
     Post.create(content: params[:content]) #contentがテーブルのカラム名、params[:content]はparamsとして送られてきたデータ
+    redirect_to action: :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'#  [HTTPメソッド] '[URIパターン]', to: '[コントローラー名]#[アクション名]' #postsというパスに、GETメソッドでリクエストが送られてきたとき、postsコントローラーのindexアクションを呼び出すという意味
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'#  [HTTPメソッド] '[URIパターン]', to: '[コントローラー名]#[アクション名]' #postsというパスに、GETメソッドでリクエストが送られてきたとき、postsコントローラーのindexアクションを呼び出すという意味
+  
   post 'posts', to: 'posts#create'
 end
