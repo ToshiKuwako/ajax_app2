e38d128fb56f2f473978083c37057c8afdcc6e70
ToshiKuwako <toshiki.se@gmail.com>
2020-08-29 16:12:45 +0900

    既読機能サーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,22 @@ class PostsController < ApplicationController
   def index #indexアクションの定義
     @posts = Post.all.order(id: "DESC")
   end
-
   
-
   def create
     Post.create(content: params[:content]) #contentがテーブルのカラム名、params[:content]はparamsとして送られてきたデータ
     redirect_to action: :index
   end
+  
+  def checked
+    post = Post.find(params[:id])
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'#  [HTTPメソッド] '[URIパターン]', to: '[コントローラー名]#[アクション名]' #postsというパスに、GETメソッドでリクエストが送られてきたとき、postsコントローラーのindexアクションを呼び出すという意味
-  
+  root to: 'posts#index'#  [HTTPメソッド] '[URIパターン]', to: '[コントローラー名]#[アクション名]' #postsというパスに、GETメソッドでリクエストが送られてきたとき、postsコントローラーのindexアクションを呼び出すという意味  
   post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#checked'
 end
